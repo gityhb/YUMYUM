28e1fefaa10897d9bd5ca8119ee126df370a0cc9
hyebin <qldpdlvl0206@naver.com>
2023-11-25 23:23:18 +0900

    yhb 11/25 23:23

--- app/src/main/java/com/example/yumyum/CommunityenrollActivity.kt
+++ app/src/main/java/com/example/yumyum/CommunityenrollActivity.kt
@@ -1,11 +1,19 @@
 package com.example.yumyum
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class CommunityenrollActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_communityenroll)
+
+        val imageView: ImageView = findViewById(R.id.back2002)
+
+        imageView.setOnClickListener {
+            startActivity(Intent(this@CommunityenrollActivity, CommunityActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/yumyum/WriteActivity.kt
+++ app/src/main/java/com/example/yumyum/WriteActivity.kt
@@ -1,11 +1,19 @@
 package com.example.yumyum
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class WriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+
+        val imageView: ImageView = findViewById(R.id.back2001)
+
+        imageView.setOnClickListener {
+            startActivity(Intent(this@WriteActivity, CommunityActivity::class.java))
+        }
     }
 }

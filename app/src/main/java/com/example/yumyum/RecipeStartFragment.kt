8ce28542e3c57b90fcd955f3b24496d96fbf5c59
soobin0205 <ss1817278@gmail.com>
2023-11-30 13:01:03 +0900

    ssb 11/30 13:00

--- app/src/main/java/com/example/yumyum/RecipeStartFragment.kt
+++ app/src/main/java/com/example/yumyum/RecipeStartFragment.kt
@@ -1,10 +1,10 @@
 package com.example.yumyum
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,7 +13,7 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [RecipeStartFragment.newInstance] factory method to
+ * Use the [RecipeContinueFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class RecipeStartFragment : Fragment() {
@@ -34,7 +34,7 @@ class RecipeStartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_start, container, false)
+        return inflater.inflate(R.layout.fragment_recipe_continue, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class RecipeStartFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment RecipeStartFragment.
+         * @return A new instance of fragment RecipeContinueFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            RecipeStartFragment().apply {
+            RecipeContinueFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)

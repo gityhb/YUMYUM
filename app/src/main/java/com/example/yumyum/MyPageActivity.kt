846bb35f88c760a3792ef22052f1215c8ba53a3f
soobin0205 <ss1817278@gmail.com>
2023-12-04 21:08:56 +0900

    ssb 12/04 21:08

--- app/src/main/java/com/example/yumyum/MyPageActivity.kt
+++ app/src/main/java/com/example/yumyum/MyPageActivity.kt
@@ -2,7 +2,6 @@ package com.example.yumyum
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayout
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,16 +9,6 @@ class MyPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_page)
 
 
-        val tab1: TabLayout.Tab = tabLayout.newTab()
-        tab1.text="찜한 레시피"
-        tabLayout.addTab(tab1)
 
-        val tab2: TabLayout.Tab = tabLayout.newTab()
-        tab1.text="등록한 레시피"
-        tabLayout.addTab(tab1)
-
-        val tab3: TabLayout.Tab = tabLayout.newTab()
-        tab1.text="레시피 후기"
-        tabLayout.addTab(tab1)
     }
 }

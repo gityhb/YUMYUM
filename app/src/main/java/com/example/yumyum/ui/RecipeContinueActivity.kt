06a9d565f013f5137295e4c7e1de7622898a5c5f
soobin0205 <ss1817278@gmail.com>
2023-12-04 16:39:47 +0900

    ssb 12/04 16:39

---
+++ app/src/main/java/com/example/yumyum/ui/RecipeContinueActivity.kt
@@ -0,0 +1,87 @@
+package com.example.yumyum.ui
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yumyum.R
+
+class RecipeContinueActivity : AppCompatActivity() {
+    private lateinit var timerTextView: TextView
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
+    private var timer: CountDownTimer? = null
+    private var isTimerRunning = false
+    private var remainingTimeInMillis: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_continue)
+
+        timerTextView = findViewById(R.id.timerTextView)
+        startButton = findViewById(R.id.startButton)
+        stopButton = findViewById(R.id.stopButton)
+
+        stopButton.setOnClickListener{
+            if(isTimerRunning){
+                stopTimer()
+                stopButton.text = "계속"
+            }else{
+
+                if(remainingTimeInMillis > 0){
+                    startTimer(remainingTimeInMillis)
+                    stopButton.text = "중지"
+                }
+            }
+        }
+
+        startButton.setOnClickListener {
+            if (remainingTimeInMillis > 0) {
+                // 초기화 버튼으로 변경
+                resetTimer()
+            } else {
+                // 새로운 타이머 시작
+                startTimer(12 * 60 * 1000) // 5분을 설정하려면 여기 값을 조정
+            }
+        }
+    }
+
+
+    private fun startTimer(durationInMillis: Long) {
+        timer = object : CountDownTimer(durationInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                remainingTimeInMillis = millisUntilFinished
+                updateTimerText()
+            }
+
+            override fun onFinish() {
+                isTimerRunning = false
+                startButton.text = "시작"
+            }
+        }.start()
+
+        isTimerRunning = true
+        startButton.text = "초기화"
+    }
+
+    private fun stopTimer() {
+        timer?.cancel()
+        isTimerRunning = false
+    }
+
+    private fun resetTimer() {
+        timer?.cancel()
+        remainingTimeInMillis = 0
+        updateTimerText()
+        isTimerRunning = false
+        startButton.text = "시작"
+    }
+
+    private fun updateTimerText() {
+        val minutes = (remainingTimeInMillis / 1000 / 60).toInt()
+        val seconds = (remainingTimeInMillis / 1000 % 60).toInt()
+        val timeText = String.format("%02d:%02d", minutes, seconds)
+        timerTextView.text = timeText
+    }
+}
---
+++ app/src/main/java/com/example/yumyum/ui/RecipeStartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yumyum.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yumyum.R
+
+class RecipeStartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_start)
+    }
+}

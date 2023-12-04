984733b7b8ee7129de62a1cfebbe214fa9822c38
soobin0205 <ss1817278@gmail.com>
2023-12-05 02:36:40 +0900

    ssb 12/05 02:36

--- app/src/main/java/com/example/yumyum/MainActivity.kt
+++ app/src/main/java/com/example/yumyum/MainActivity.kt
@@ -2,17 +2,14 @@ package com.example.yumyum
 
 import android.os.Bundle
 import android.widget.ImageView
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.yumyum.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
 private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +21,8 @@ private lateinit var binding: ActivityMainBinding
 
         val partImg: ImageView = findViewById(R.id.food1)
         partImg.clipToOutline = true
+
+
 
         binding.food1.clipToOutline = true
         binding.food2.clipToOutline = true
@@ -42,5 +41,8 @@ private lateinit var binding: ActivityMainBinding
             R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)*/
+
+
+
     }
 }

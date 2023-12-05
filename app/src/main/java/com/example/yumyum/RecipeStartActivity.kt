ca7a72be99ef04d8d1a5eda9671cd28fb824a7d8
SoJeong <misteryoh0308@gmail.com>
2023-12-05 18:12:22 +0900

    osj 12/05 18:12

--- app/src/main/java/com/example/yumyum/RecipeStartActivity.kt
+++ app/src/main/java/com/example/yumyum/RecipeStartActivity.kt
@@ -1,18 +1,17 @@
 package com.example.yumyum
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.yumyum.databinding.ActivityRecipeStartBinding
 
 class RecipeStartActivity : AppCompatActivity() {
+    lateinit var binding : ActivityRecipeStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipe_start)
+        binding = ActivityRecipeStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recipe = intent.getStringExtra("Recipe")
+        /*val recipe = intent.getStringExtra("Recipe")
         val ingredient = intent.getStringExtra("Ingredient")
         val time = intent.getStringExtra("Time")
 
@@ -22,16 +21,23 @@ class RecipeStartActivity : AppCompatActivity() {
 
         start_name.text = recipe
         start_ingredient.text = ingredient
-        start_time.text = time
+        start_time.text = time*/
 
-        val recipe_start: Button = findViewById(R.id.StartButton)
-        recipe_start.setOnClickListener {
-            startActivity(Intent(this, RecipeContinueActivity::class.java))
+        var cook_imgV : ImageView = findViewById(R.id.cook_img)
+        var cook_titleV : TextView = findViewById(R.id.cook_title)
+        var cook_userV : TextView = findViewById(R.id.enroll_user)
+        var cook_timeV : TextView = findViewById(R.id.time)
+        var cook_ingredient_detailV : TextView = findViewById(R.id.cook_ingredient_detail)
+        var cook_tool_detail : TextView = findViewById(R.id.cook_tool_detail)
+        /*var cook*/
+
+        binding.likeYes.setOnClickListener {
+            binding.likeYes.visibility = View.GONE
+            binding.likeNo.visibility = View.VISIBLE
         }
-
-        val backButton: ImageView = findViewById(R.id.back_btn)
-        backButton.setOnClickListener {
-            startActivity(Intent(this, RecipeSearchActivity::class.java))
+        binding.likeNo.setOnClickListener{
+            binding.likeNo.visibility = View.GONE
+            binding.likeYes.visibility = View.VISIBLE
         }
     }
 }

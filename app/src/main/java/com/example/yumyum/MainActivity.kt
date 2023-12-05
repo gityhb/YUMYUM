80abd9baffe0168262b4ce976a40529e678f343f
SoJeong <misteryoh0308@gmail.com>
2023-12-05 21:02:32 +0900

    osj 12/05 21:00

--- app/src/main/java/com/example/yumyum/MainActivity.kt
+++ app/src/main/java/com/example/yumyum/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.yumyum
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.yumyum.databinding.ActivityMainBinding
 

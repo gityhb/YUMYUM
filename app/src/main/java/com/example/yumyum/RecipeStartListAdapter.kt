4a44d6e554585756d2ffff93dbbaa91c9f81bb42
SoJeong <misteryoh0308@gmail.com>
2023-12-05 18:15:18 +0900

    osj 12/05 18:15

--- app/src/main/java/com/example/yumyum/RecipeStartListAdapter.kt
+++ app/src/main/java/com/example/yumyum/RecipeStartListAdapter.kt
@@ -1,11 +1,9 @@
 package com.example.yumyum
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
+/*
 class RecipeStartListAdapter(val cookList: List<CommentItem>) :
-/*    override fun onCreate(savedInstanceState: Bundle?) {
+  override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_start_list_adapter)
-    }*/
-}
+    }
+}*/

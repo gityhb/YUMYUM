0a3e096c1520e1914e8f48f7fa137824d6ddec95
SoJeong <misteryoh0308@gmail.com>
2023-12-07 16:58:13 +0900

    osj 12/07 16:57

--- app/src/main/java/com/example/yumyum/SidebarFragment.kt
+++ app/src/main/java/com/example/yumyum/SidebarFragment.kt
@@ -5,18 +5,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.example.yumyum.databinding.FragmentSidebarBinding
 
 class SidebarFragment : Fragment() {
-
+    lateinit var binding : FragmentSidebarBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_sidebar, container, false)
+        binding = FragmentSidebarBinding.inflate(inflater, container, false)
+        val view = binding.root
 
-        view.findViewById<ImageView>(R.id.cancel_img).setOnClickListener {
+        val dbHelper = DBHelper(requireContext())
+        val db = dbHelper.readableDatabase
+
+        if(dbHelper.isUserLoggedIn()) {
+            val userInfo = dbHelper.getUserInfo(db)
+
+            if(userInfo != null) { // 사용자 정보가 있는 경우
+                binding.userNk.text = "${userInfo?.nickname}"
+            }
+            db.close()
+        }
+
+        binding.cancelImg.setOnClickListener {
             val mainActivityIntent = Intent(requireContext(), MainActivity::class.java)
             startActivity(mainActivityIntent)
         }
+
+
         return view
 
     }
